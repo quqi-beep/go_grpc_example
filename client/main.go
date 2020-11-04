4c63e3f393e8f454d54631bbc700de12d14cbf8b
quqi <quqi@fnlinker.com>
2020-11-04 17:03:49 +0800

    Grpc双向流模式

--- client/main.go
+++ client/main.go
@@ -4,6 +4,7 @@ import (
 	"client/pb/number"
 	"context"
 	"fmt"
+	"io"
 	"strconv"
 
 	"google.golang.org/grpc"
@@ -22,16 +23,52 @@ func main() {
 	//2.连接Grpc
 	c := number.NewOprationNumberServiceClient(conn)
 
-	//3.创建要发送的结构体
+	//3.调用方法(简单rpc模式)
+	addNumberAsync(c)
+
+	//4.双向流模式
+	testStreamSendRequestAsync(c)
+}
+
+//简单rpc模式
+func addNumberAsync(c number.OprationNumberServiceClient) {
+	//创建要发送的结构体
 	request := &number.AddNumberRequest{
 		First:  10,
 		Second: 11,
 	}
-	//3.调用方法
 	res, err := c.AddNumberAsync(context.Background(), request)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(res)
 	fmt.Println("服务器端返回的结果为：" + strconv.FormatInt(int64(res.Num), 10))
+}
+
+//双向流模式
+func testStreamSendRequestAsync(c number.OprationNumberServiceClient) {
+	stream, err := c.TestStreamSendRequestAsync(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 0; i < 30; i++ {
+		err = stream.Send(&number.StreamRequest{
+			Question: "客客客客客",
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Client_" + res.Answer + "_" + strconv.Itoa(i+1))
+	}
+	err = stream.CloseSend()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- client/pb/number/number.pb.go
+++ client/pb/number/number.pb.go
@@ -131,6 +131,100 @@ func (x *AddNumberResponse) GetNum() int32 {
 	return 0
 }
 
+type StreamRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Question string `protobuf:"bytes,1,opt,name=Question,proto3" json:"Question,omitempty"`
+}
+
+func (x *StreamRequest) Reset() {
+	*x = StreamRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_number_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StreamRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StreamRequest) ProtoMessage() {}
+
+func (x *StreamRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_number_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StreamRequest.ProtoReflect.Descriptor instead.
+func (*StreamRequest) Descriptor() ([]byte, []int) {
+	return file_number_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *StreamRequest) GetQuestion() string {
+	if x != nil {
+		return x.Question
+	}
+	return ""
+}
+
+type StreamResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Answer string `protobuf:"bytes,1,opt,name=Answer,proto3" json:"Answer,omitempty"`
+}
+
+func (x *StreamResponse) Reset() {
+	*x = StreamResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_number_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StreamResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StreamResponse) ProtoMessage() {}
+
+func (x *StreamResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_number_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StreamResponse.ProtoReflect.Descriptor instead.
+func (*StreamResponse) Descriptor() ([]byte, []int) {
+	return file_number_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *StreamResponse) GetAnswer() string {
+	if x != nil {
+		return x.Answer
+	}
+	return ""
+}
+
 var File_number_proto protoreflect.FileDescriptor
 
 var file_number_proto_rawDesc = []byte{
@@ -141,15 +235,25 @@ var file_number_proto_rawDesc = []byte{
 	0x12, 0x16, 0x0a, 0x06, 0x53, 0x65, 0x63, 0x6f, 0x6e, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05,
 	0x52, 0x06, 0x53, 0x65, 0x63, 0x6f, 0x6e, 0x64, 0x22, 0x25, 0x0a, 0x11, 0x41, 0x64, 0x64, 0x4e,
 	0x75, 0x6d, 0x62, 0x65, 0x72, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x10, 0x0a,
-	0x03, 0x4e, 0x75, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x03, 0x4e, 0x75, 0x6d, 0x32,
-	0x5e, 0x0a, 0x15, 0x4f, 0x70, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x4e, 0x75, 0x6d, 0x62, 0x65,
-	0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x45, 0x0a, 0x0e, 0x41, 0x64, 0x64, 0x4e,
-	0x75, 0x6d, 0x62, 0x65, 0x72, 0x41, 0x73, 0x79, 0x6e, 0x63, 0x12, 0x18, 0x2e, 0x6e, 0x75, 0x6d,
-	0x62, 0x65, 0x72, 0x2e, 0x41, 0x64, 0x64, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x1a, 0x19, 0x2e, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x2e, 0x41, 0x64,
-	0x64, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42,
-	0x0a, 0x5a, 0x08, 0x2e, 0x3b, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x03, 0x4e, 0x75, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x03, 0x4e, 0x75, 0x6d, 0x22,
+	0x2b, 0x0a, 0x0d, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x12, 0x1a, 0x0a, 0x08, 0x51, 0x75, 0x65, 0x73, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x08, 0x51, 0x75, 0x65, 0x73, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x28, 0x0a, 0x0e,
+	0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16,
+	0x0a, 0x06, 0x41, 0x6e, 0x73, 0x77, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06,
+	0x41, 0x6e, 0x73, 0x77, 0x65, 0x72, 0x32, 0xaf, 0x01, 0x0a, 0x15, 0x4f, 0x70, 0x72, 0x61, 0x74,
+	0x69, 0x6f, 0x6e, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65,
+	0x12, 0x45, 0x0a, 0x0e, 0x41, 0x64, 0x64, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x41, 0x73, 0x79,
+	0x6e, 0x63, 0x12, 0x18, 0x2e, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x2e, 0x41, 0x64, 0x64, 0x4e,
+	0x75, 0x6d, 0x62, 0x65, 0x72, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x19, 0x2e, 0x6e,
+	0x75, 0x6d, 0x62, 0x65, 0x72, 0x2e, 0x41, 0x64, 0x64, 0x4e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x4f, 0x0a, 0x1a, 0x54, 0x65, 0x73, 0x74, 0x53,
+	0x74, 0x72, 0x65, 0x61, 0x6d, 0x53, 0x65, 0x6e, 0x64, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
+	0x41, 0x73, 0x79, 0x6e, 0x63, 0x12, 0x15, 0x2e, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x2e, 0x53,
+	0x74, 0x72, 0x65, 0x61, 0x6d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16, 0x2e, 0x6e,
+	0x75, 0x6d, 0x62, 0x65, 0x72, 0x2e, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x28, 0x01, 0x30, 0x01, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x3b, 0x6e, 0x75,
+	0x6d, 0x62, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -164,16 +268,20 @@ func file_number_proto_rawDescGZIP() []byte {
 	return file_number_proto_rawDescData
 }
 
-var file_number_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_number_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_number_proto_goTypes = []interface{}{
 	(*AddNumberRequest)(nil),  // 0: number.AddNumberRequest
 	(*AddNumberResponse)(nil), // 1: number.AddNumberResponse
+	(*StreamRequest)(nil),     // 2: number.StreamRequest
+	(*StreamResponse)(nil),    // 3: number.StreamResponse
 }
 var file_number_proto_depIdxs = []int32{
 	0, // 0: number.OprationNumberService.AddNumberAsync:input_type -> number.AddNumberRequest
-	1, // 1: number.OprationNumberService.AddNumberAsync:output_type -> number.AddNumberResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 1: number.OprationNumberService.TestStreamSendRequestAsync:input_type -> number.StreamRequest
+	1, // 2: number.OprationNumberService.AddNumberAsync:output_type -> number.AddNumberResponse
+	3, // 3: number.OprationNumberService.TestStreamSendRequestAsync:output_type -> number.StreamResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -209,6 +317,30 @@ func file_number_proto_init() {
 				return nil
 			}
 		}
+		file_number_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StreamRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_number_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StreamResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -216,7 +348,7 @@ func file_number_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_number_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -243,6 +375,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type OprationNumberServiceClient interface {
 	AddNumberAsync(ctx context.Context, in *AddNumberRequest, opts ...grpc.CallOption) (*AddNumberResponse, error)
+	TestStreamSendRequestAsync(ctx context.Context, opts ...grpc.CallOption) (OprationNumberService_TestStreamSendRequestAsyncClient, error)
 }
 
 type oprationNumberServiceClient struct {
@@ -262,9 +395,41 @@ func (c *oprationNumberServiceClient) AddNumberAsync(ctx context.Context, in *Ad
 	return out, nil
 }
 
+func (c *oprationNumberServiceClient) TestStreamSendRequestAsync(ctx context.Context, opts ...grpc.CallOption) (OprationNumberService_TestStreamSendRequestAsyncClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_OprationNumberService_serviceDesc.Streams[0], "/number.OprationNumberService/TestStreamSendRequestAsync", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &oprationNumberServiceTestStreamSendRequestAsyncClient{stream}
+	return x, nil
+}
+
+type OprationNumberService_TestStreamSendRequestAsyncClient interface {
+	Send(*StreamRequest) error
+	Recv() (*StreamResponse, error)
+	grpc.ClientStream
+}
+
+type oprationNumberServiceTestStreamSendRequestAsyncClient struct {
+	grpc.ClientStream
+}
+
+func (x *oprationNumberServiceTestStreamSendRequestAsyncClient) Send(m *StreamRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *oprationNumberServiceTestStreamSendRequestAsyncClient) Recv() (*StreamResponse, error) {
+	m := new(StreamResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // OprationNumberServiceServer is the server API for OprationNumberService service.
 type OprationNumberServiceServer interface {
 	AddNumberAsync(context.Context, *AddNumberRequest) (*AddNumberResponse, error)
+	TestStreamSendRequestAsync(OprationNumberService_TestStreamSendRequestAsyncServer) error
 }
 
 // UnimplementedOprationNumberServiceServer can be embedded to have forward compatible implementations.
@@ -273,6 +438,9 @@ type UnimplementedOprationNumberServiceServer struct {
 
 func (*UnimplementedOprationNumberServiceServer) AddNumberAsync(context.Context, *AddNumberRequest) (*AddNumberResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddNumberAsync not implemented")
+}
+func (*UnimplementedOprationNumberServiceServer) TestStreamSendRequestAsync(OprationNumberService_TestStreamSendRequestAsyncServer) error {
+	return status.Errorf(codes.Unimplemented, "method TestStreamSendRequestAsync not implemented")
 }
 
 func RegisterOprationNumberServiceServer(s *grpc.Server, srv OprationNumberServiceServer) {
@@ -297,6 +465,32 @@ func _OprationNumberService_AddNumberAsync_Handler(srv interface{}, ctx context.
 	return interceptor(ctx, in, info, handler)
 }
 
+func _OprationNumberService_TestStreamSendRequestAsync_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(OprationNumberServiceServer).TestStreamSendRequestAsync(&oprationNumberServiceTestStreamSendRequestAsyncServer{stream})
+}
+
+type OprationNumberService_TestStreamSendRequestAsyncServer interface {
+	Send(*StreamResponse) error
+	Recv() (*StreamRequest, error)
+	grpc.ServerStream
+}
+
+type oprationNumberServiceTestStreamSendRequestAsyncServer struct {
+	grpc.ServerStream
+}
+
+func (x *oprationNumberServiceTestStreamSendRequestAsyncServer) Send(m *StreamResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *oprationNumberServiceTestStreamSendRequestAsyncServer) Recv() (*StreamRequest, error) {
+	m := new(StreamRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _OprationNumberService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "number.OprationNumberService",
 	HandlerType: (*OprationNumberServiceServer)(nil),
@@ -306,6 +500,13 @@ var _OprationNumberService_serviceDesc = grpc.ServiceDesc{
 			Handler:    _OprationNumberService_AddNumberAsync_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "TestStreamSendRequestAsync",
+			Handler:       _OprationNumberService_TestStreamSendRequestAsync_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "number.proto",
 }
